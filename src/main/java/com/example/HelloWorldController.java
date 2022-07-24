2fe049a33295d420c110b211c703a6d4b7c6f9af
Fisnik Shabani <fisnik.shabani@gmail.com>
2022-07-25 01:17:54 +0200

    Added new logger

--- src/main/java/com/example/HelloWorldController.java
+++ src/main/java/com/example/HelloWorldController.java
@@ -3,20 +3,21 @@ package com.example;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller("/hello")
 public class HelloWorldController {
 
-
+    private static final Logger LOG = LoggerFactory.getLogger(HelloWorldController.class);
     private final MyService service;
 
     public HelloWorldController(MyService service) {
         this.service = service;
     }
-
-
     @Get(produces = MediaType.TEXT_PLAIN)
     public String  helloWorld(){
+        LOG.debug("Called the Hello World API.");
         return service.helloFromService();
     }
 }
